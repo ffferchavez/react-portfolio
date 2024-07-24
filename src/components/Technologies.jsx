c5a8828b945669c17e7fc2ef05d3fc057e577905
ffferchavez <ffferchavez@gmail.com>
2024-07-24 21:30:18 +0200

    added my toolbox link under technologies

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,3 +1,4 @@
+import { useState, useRef, useEffect } from "react";
 import { RiReactjsLine } from "react-icons/ri";
 import { SiMongodb, SiPython, SiPostman, SiFigma, SiGithub } from "react-icons/si";
 import { FaNodeJs, FaAngular } from "react-icons/fa";
@@ -29,7 +30,40 @@ const techs = [
   { Icon: SiFigma, name: "Figma", color: "text-white" },
 ];
 
+const additionalTools = [
+  "React Native", "Angular Material", "ExpressJS",
+  "Bootstrap", "Sass", "JQuery", "Heroku",
+  "Netlify", "AWS Lambda", "OAuth 2.0", "Recharts",
+  "Jest", "Cucumber", "Puppeteer", "PWA", "TDD", "BDD",
+  "OOP", "Gherkin", "Django", "Docker", "Firebase", "AI", "Trello",
+  "Jira", "Canva", "Xcode", "Android Studio", "Scrum", "Kanban"
+];
+
 const Technologies = () => {
+  const [showAdditionalTools, setShowAdditionalTools] = useState(false);
+  const listRef = useRef(null);
+
+  const toggleAdditionalTools = () => {
+    setShowAdditionalTools(!showAdditionalTools);
+  };
+
+  const closeAdditionalTools = () => {
+    setShowAdditionalTools(false);
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (listRef.current && !listRef.current.contains(event.target)) {
+        closeAdditionalTools();
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
   return (
     <div className="border-b border-neutral-800 pb-24">
       <motion.h2
@@ -64,6 +98,42 @@ const Technologies = () => {
           </motion.div>
         ))}
       </motion.div>
+      <div className="text-center mt-8">
+        <motion.a
+          onClick={toggleAdditionalTools}
+          style={{ color: '#FFFFFF', textDecoration: 'none', cursor: 'pointer', display: 'block', marginTop: '10px' }}
+          whileHover={{ color: "#48CAE4" }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          more of my toolbox
+        </motion.a>
+        {showAdditionalTools && (
+          <div ref={listRef} className="relative inline-block mt-4">
+            <motion.div
+              className="relative mt-4 text-center p-6 rounded shadow-lg"
+              style={{ backgroundColor: 'rgba(35, 35, 35, 0.3)' }}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 0.5 }}
+            >
+              <button
+                onClick={closeAdditionalTools}
+                className="absolute top-2 right-2 text-gray-400"
+                style={{ cursor: 'pointer' }}
+              >
+                &times;
+              </button>
+              <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                {additionalTools.map((tool, index) => (
+                  <div key={index} className="col-span-1">{tool}</div>
+                ))}
+              </div>
+            </motion.div>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
